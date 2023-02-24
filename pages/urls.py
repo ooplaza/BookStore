6854b776154dd51db378d4376c574776f97d429b
0R1-qt <orly.plaza@hcdc.edu.ph>
2023-02-25 02:51:46 +0800

    Bootstrap:Added | PagesAppTests:Added

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name='Home'),
+    path("about/", views.AboutPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 class HomePageView(TemplateView):
+    """ Renders the home.html using TemplateView. """
     template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    """ Renders the about.html using TemplateView. """
+    template_name = "about.html"
